2e58a65702a3237aa15f54c340533b8622dd549c
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-16 07:39:57 +0100

    add markers without third party component

---
+++ data/CPTmarkers.js
@@ -0,0 +1,36 @@
+
+var CPTmarkers = {
+  interest : [{
+    coords : [18.43, -34.19],
+    name : "Simonstown",
+    text : "Bla Bla Bla"
+  },
+  {
+    coords : [18.41, -33.90],
+    name : "Green Point Stadium",
+    text : "Bla Bla Bla"
+  },
+  {
+    coords : [18.41, -33.96],
+    name : "Table Mountain",
+    text : "Bla Bla Bla"
+  },
+  {
+    coords : [18.38, -33.93],
+    name : "Lion's Head",
+    text : "Bla Bla Bla"
+  }],
+  wedding :
+    [{
+      coords : [18.87, -33.83],
+      name : "Natte Valleij",
+      text : " Bla Bla Bla"
+    },
+    {
+      coords : [18.43, -33.93],
+      name : "Taxi drop-off",
+      text : " Bla Bla Bla"
+    }],
+}
+
+export default CPTmarkers
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,27 +2,48 @@ import React from 'react'
 import mapboxgl from 'mapbox-gl'
 import Styles from './MapStyles.module.css'
 import Button from '../Button/Button'
+import Marker from './Marker'
+import CPTmarkers from '../../../data/CPTmarkers.js'
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q';
 
 class Map extends React.Component{
   // mapRef = React.createRef();
   map;
+  Ref10;
   state = {
     lat: -34.0906,
     lng: 18.5474,
-    zoom: 9.23
+    zoom: 9.23,
+    interest: true,
+    wedding: false,
+    data: null
   }
 
   componentDidMount(){
-    let {lng, lat, zoom} = this.state;
+    let {lng, lat, zoom, interest, wedding, ids} = this.state;
     console.log("MOUNTED AGAIN")
-    console.log(lng)
+    console.log(CPTmarkers.interest)
     this.map = new mapboxgl.Map({
           container: this.mapContainer, // container id
           style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
           center: [lng, lat], // starting position [lng, lat]
           zoom: zoom // starting zoom
           });
+
+      let data = CPTmarkers.interest;
+      let newData=[];
+      data.forEach(ob => {
+        let ref = React.createRef();
+        ob.ref = ref;
+        console.log(ob.ref)
+        newData.push(ob);
+        var marker = new mapboxgl.Marker(ref)
+              .setLngLat(ob.coords)
+              .addTo(this.map);
+      })
+
+    this.setState({data: newData})
+
 
     this.map.on("move", () => {
       let {lng, lat} = this.map.getCenter();
@@ -31,10 +52,6 @@ class Map extends React.Component{
         lng : lng.toFixed(4),
         zoom: this.map.getZoom().toFixed(2)
       })
-
-      console.log("COORDS")
-      console.log(lng.toFixed(4) + " - " + lat.toFixed(4))
-      console.log(this.map.getZoom().toFixed(2))
     })
 
   }
@@ -55,8 +72,6 @@ class Map extends React.Component{
       break;
       default: console.log("Coords incorrect")
     }
-    console.log("updated coords")
-    console.log(coords)
 
     this.setState({
       lat: coords.lat,
@@ -71,14 +86,30 @@ class Map extends React.Component{
 
 
   render(){
+    let markers;
+    if(this.state.data){
+      markers = this.state.data.map(marker => {
+         return <div className={Styles.marker} ref={marker.ref}></div>
+     })
+    }
+
+    let  teste = () => {
+        console.log("yay")
+        return <p> Helloooo </p>
+      }
+
+
     return (
       <div className={Styles.mapContainer}>
-        <div className={Styles.map} ref={el => this.mapContainer = el}></div>
+        <div className={Styles.map} ref={el => this.mapContainer = el}>
+          {markers}
+        </div>
         <div className={Styles.mapInfo}>
           <h3> Let's explore </h3>
           <div className={Styles.flexCenterCenter}>
             <Button clickHandler={this.clickHandler} text="Cape Town" id="CPT"  />
             <Button clickHandler={this.clickHandler} text="South Africa" id="SA" />
+            {teste()}
           </div>
         </div>
       </div>
