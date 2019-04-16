8568db776b73bde4d050475b7a18fbf0c8980611
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-16 19:42:41 +0100

    create map button active classes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Styles from './ButtonStyles.module.css'
 
-const Button = (props) => {
+const Button = (props) =>{
+    let activeClass;
+    let activeState = props.active;
+    activeState ? activeClass= Styles.active : activeClass= Styles.unactive;
 
-  return (
-    <button id={props.id} onClick={props.clickHandler}>{props.text}</button>
-  )
+    return (
+      <button className={[Styles.button, activeClass].join(' ')} id={props.id} onClick={ e => props.clickHandler(e)}>{props.text}</button>
+    )
+
 }
 
 export default Button
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -14,26 +14,85 @@ const MapBox = ReactMapboxGl({
 class Map extends React.Component{
 
   state = {
-       width: 400,
-       height: 400,
        lat: -34.0906,
        lng: 18.5474,
        zoom: 9.23,
+       type: null,
+       currentArea: 'CPT',
+       currentMarker: null
+  }
+
+  componentDidMount(){
+    this.setState({markers: [...CPTmarkers.interest]})
+  }
+
+  clickHandler = (e) => {
+    let btn = e.target.id;
+    let area;
+    let coords = {}
+    switch(btn){
+      case 'CPT' :
+        coords.lng = 18.5474;
+        coords.lat = -34.0906;
+        coords.zoom = 9.23;
+        area = 'CPT'
+      break;
+      case 'SA' :
+        coords.lng = 24.4919;
+        coords.lat = -30.6245;
+        coords.zoom = 5.02;
+        area = 'SA'
+      break;
+      default: console.log("Coords incorrect")
+    }
+
+    this.setState({
+      lat: coords.lat,
+      lng : coords.lng,
+      zoom: coords.zoom,
+      currentArea: area
+    })
+
+  }
+
+  showInfo(marker){
+    this.setState({currentMarker: marker})
   }
 
   render(){
-    let {lng, lat, zoom} = this.state;
+    let {lng, lat, zoom, currentMarker, currentArea} = this.state;
+
+    if (!currentMarker) currentMarker = '';
     let coords = [lng, lat]
-    let zoomValue = [zoom]
+    let zoomValue = [zoom];
+    let markers;
+    if(this.state.markers){
+      markers = CPTmarkers.interest.map((marker, i) => {
+          return <Feature coordinates={marker.coords}
+                          onClick={() =>this.showInfo(marker)}
+                          properties={marker.name} />
+        })
+    }
+
+
+
     return (
       <div className={Styles.mapContainer}>
-        <MapBox movingMethod='flyTo' className={Styles.map} style="mapbox://styles/mapbox/streets-v8" center={coords} zoom={zoomValue} />
+        <MapBox movingMethod='flyTo' className={Styles.map} style="mapbox://styles/mapbox/streets-v8" center={coords} zoom={zoomValue}>
+          <Layer type="symbol" layout={{"icon-image": "marker-15", "icon-size": 1.6}} paint={{"icon-color": "#c70202"}}>
+            {markers}
+          </Layer>
+        </MapBox>
         <div className={Styles.mapInfo}>
-          <h3> Let's explore </h3>
-          <div className={Styles.flexCenterCenter}>
-            <Button clickHandler={this.clickHandler} text="Cape Town" id="CPT"  />
-            <Button clickHandler={this.clickHandler} text="South Africa" id="SA" />
+          <div className={Styles.mapSelector}>
+            <h2> Let's explore </h2>
+            <div className={Styles.flexCenterCenter}>
+              <Button active={currentArea === 'CPT'} clickHandler={this.clickHandler} text="Cape Town" id="CPT"  />
+              <Button active={currentArea === 'SA'} clickHandler={this.clickHandler} text="South Africa" id="SA" />
+            </div>
           </div>
+          <h3> {currentMarker.name} </h3>
+          <p> {currentMarker.text} </p>
         </div>
       </div>
     )
