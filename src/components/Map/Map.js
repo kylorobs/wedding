44c7a089846b23148447d91986704f723dcdadef
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-16 10:47:29 +0100

    mapbox library

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,9 +6,12 @@ import Button from '../Button/Button'
 import Marker from './Marker'
 import CPTmarkers from '../../../data/CPTmarkers.js'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+
+const MapBox = ReactMapboxGl({
+  accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'})
 
 class Map extends React.Component{
-  // mapRef = React.createRef();
 
   state = {
      viewport: {
@@ -24,13 +27,12 @@ class Map extends React.Component{
 
     return (
       <div className={Styles.mapContainer}>
-      <ReactMapGL mapboxApiAccessToken={'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'} {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}/>
+        <MapBox style="mapbox://styles/mapbox/streets-v8" center=[18.5474, -340906] zoom=[9.23] />
         <div className={Styles.mapInfo}>
           <h3> Let's explore </h3>
           <div className={Styles.flexCenterCenter}>
             <Button clickHandler={this.clickHandler} text="Cape Town" id="CPT"  />
             <Button clickHandler={this.clickHandler} text="South Africa" id="SA" />
-
           </div>
         </div>
       </div>
