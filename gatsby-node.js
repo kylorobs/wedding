3c23d9a9d2f92bf1e4888ae820cd92cba8ca217f
Kyle Robinson <kylerobbo12@gmail.com>
2019-07-08 19:32:08 +0100

    gatsby node mapbox patch

---
+++ gatsby-node.js
@@ -0,0 +1,13 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /mapbox-gl/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
