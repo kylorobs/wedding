494938525288114abae1ffcad7cd7d469070ec87
Kyle Robinson <kylerobbo12@gmail.com>
2019-07-09 12:56:35 +0100

    move key to render statement

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,7 +5,6 @@ import CPTmarkers from '../../data/CPTmarkers';
 import ReactMapboxGl from 'react-mapbox-gl';
 import { Layer, Feature } from 'react-mapbox-gl';
 
-const MapBox = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'})
 
 class Map extends React.Component{
 
@@ -56,6 +55,7 @@ class Map extends React.Component{
   }
 
   render(){
+    const MapBox = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'})
     let {lng, lat, zoom, currentMarker, currentArea} = this.state;
 
     if (!currentMarker) currentMarker = '';
