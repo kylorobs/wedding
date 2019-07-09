04524eed2b6d8e64fe4e8bf6a4bde79b63b86c82
Kyle Robinson <kylerobbo12@gmail.com>
2019-07-09 16:39:47 +0100

    add logic inside map component to test debugging

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,6 +4,25 @@ import Button from '../Button/Button';
 import CPTmarkers from '../../data/CPTmarkers';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
+let mapboxgl;
+let ReactMapboxGl = {};
+
+if (typeof window !== `undefined`) {
+  mapboxgl = require('mapbox-gl')
+  ReactMapboxGl = require('react-mapbox-gl')
+} else {
+  ReactMapboxGl.Map = () => {
+    return class Mock extends React.Component {
+      constructor() {
+        super()
+      }
+      render() {
+        return <div />
+      }
+    }
+  }
+}
+
 const MapBox = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'})
 
 class MapCanvas extends React.Component{
