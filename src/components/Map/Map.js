51b1a55df841f3ca0a93884f41ace3e5448462db
Kyle Robinson <kylerobbo12@gmail.com>
2019-07-09 12:52:37 +0100

    separate imports

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,12 +2,10 @@ import React from 'react'
 import Styles from './MapStyles.module.css'
 import Button from '../Button/Button';
 import CPTmarkers from '../../data/CPTmarkers';
-// import 'mapbox-gl/dist/mapbox-gl.css';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl from 'react-mapbox-gl';
+import { Layer, Feature } from 'react-mapbox-gl';
 
-const MapBox = ReactMapboxGl({
-  accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'
-})
+const MapBox = ReactMapboxGl({accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'})
 
 class Map extends React.Component{
 
