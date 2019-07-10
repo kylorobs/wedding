80b916f34e32b1c49670bef0b95b20fa0d39e500
Kyle Robinson <kylerobbo12@gmail.com>
2019-07-10 08:16:27 +0100

    change name to map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -28,7 +28,7 @@ import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 const MapBox = ReactMapboxGl.Map({accessToken: 'pk.eyJ1Ijoia3lsZXJvYmluc29uIiwiYSI6ImNqdWd3cjZ3cDAwZnozem1vejMyM241NDYifQ.L7UNeZAlJ_its-x60b9L3Q'});
 
-class MapCanvas extends React.Component{
+class Map extends React.Component{
 
   state = {
        lat: -34.0906,
@@ -126,4 +126,4 @@ class MapCanvas extends React.Component{
   }
 }
 
-export default MapCanvas;
+export default Map;
