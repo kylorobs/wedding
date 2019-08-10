cb5b1808ff0d62df2e58362b4d8b62e658ab0cea
Kyle Robinson <kylerobbo12@gmail.com>
2019-08-10 20:43:16 +0100

    gift wording

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -12,7 +12,7 @@ class thankyou extends React.Component {
                         <div style={{display: 'flex', flexFlow: 'column'}}>
                             <h1> Thanks for the RSVP!</h1>
                             <p>If you're able to make it, we cannot wait to see you!</p>
-                            <p> All we can possibly ask for is your presence on our special day, but if you would really like to give us a gift, we would greatly appreciate a small contribution to <a href="https://www.weddingshop.com/Buy/View/63803"> help start our new chapter in Australia.</a> </p>
+                            <p> Your presence on our special day is more than enough, but if you would really like to give us a gift, we would greatly appreciate a small contribution to <a href="https://www.weddingshop.com/Buy/View/63803"> help start our new chapter in Australia.</a> </p>
                             <div style={{width: '15em', margin: 'auto'}}>
                             </div>
                         </div>
